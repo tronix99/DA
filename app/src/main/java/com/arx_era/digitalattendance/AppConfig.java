963b7720d0a3b314fa3543c6e03d5a5b9d7e0ddc
Tronix99 <rajdevkar.6@gmail.com>
2017-07-11 17:03:04 +0530

    Login Done

---
+++ app/src/main/java/com/arx_era/digitalattendance/AppConfig.java
@@ -0,0 +1,12 @@
+package com.arx_era.digitalattendance;
+
+/**
+ * Created by Tronix99 on 06-07-2017.
+ */
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "https://arxtest.000webhostapp.com/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "https://arxtest.000webhostapp.com/register.php";
+}
